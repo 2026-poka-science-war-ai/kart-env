eb8cd4acbfe0575d599fc8c3194dcfcb99201f93
jae1u <yjw_0412@naver.com>
2026-02-20 17:49:59 +0000

    feat: Implement action packing and socket communication

--- kart_env/utils/enums.py
+++ kart_env/utils/enums.py
@@ -6,3 +6,25 @@ ISO_PATH = "/kart_env/MarioKartWii.iso"
 DOLPHIN_SETTINGS_PATH = "/kart_env/dolphin_settings"
 SCRIPT_PATH = "/kart_env/kart_env/script.py"
 SOCK_BASE = 9999
+GC_BUTTONS = ["A", "B", "X", "Y", "Z", "Start", "Up", "Down", "Left", "Right", "L", "R"]
+AGENT_STRUCT_FORMAT = "<H6f"
+NEUTRAL_ACTION = {
+    "A": 0,
+    "B": 0,
+    "X": 0,
+    "Y": 0,
+    "Z": 0,
+    "Start": 0,
+    "Up": 0,
+    "Down": 0,
+    "Left": 0,
+    "Right": 0,
+    "L": 0,
+    "R": 0,
+    "StickX": 0.0,
+    "StickY": 0.0,
+    "CStickX": 0.0,
+    "CStickY": 0.0,
+    "TriggerLeft": 0.0,
+    "TriggerRight": 0.0,
+}
