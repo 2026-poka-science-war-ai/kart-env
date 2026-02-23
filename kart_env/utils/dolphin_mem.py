f1bd85defa131c5bfc382112831582aefd2f7a33
jae1u <yjw_0412@naver.com>
2026-02-23 07:11:22 +0000

    feat: Refactor DolphinMem to use mmap for memory access

--- kart_env/utils/dolphin_mem.py
+++ kart_env/utils/dolphin_mem.py
@@ -1,55 +1,34 @@
-class DolphinMem:
-    MEM1_START = 0x80000000
-    MEM1_SIZE = 0x01800000  # 24MB
-    MEM2_START = 0x90000000
-    MEM2_SIZE = 0x04000000  # 64MB
+import mmap
+import os
 
+
+class DolphinMem:
     def __init__(self, pid: int):
         self.pid = pid
-        self.mem1_addr = 0
-        self.mem2_addr = 0
-        with open(f"/proc/{self.pid}/maps") as f:
-            for line in f:
-                if "/dev/shm/dolphin" not in line:
-                    continue
 
-                parts = line.split()
-                start_addr, end_addr = [int(x, 16) for x in parts[0].split("-")]
-                offset = int(parts[2], 16)
-                size = end_addr - start_addr
+        fd_dir = f"/proc/{self.pid}/fd"
+        target_fd = -1
+        for fd_num in os.listdir(fd_dir):
+            fd_path = os.path.join(fd_dir, fd_num)
+            if "/dev/shm/dolphin-emu" in os.readlink(fd_path):
+                with open(fd_path, "r") as f:
+                    target_fd = f.fileno()
+                    # fmt: off
+                    self.mm = mmap.mmap(target_fd, 0, flags=mmap.MAP_SHARED, prot=mmap.PROT_READ)
+                    self.mv = memoryview(self.mm)
+                    # fmt: on
+                    return
 
-                # fmt: off
-                if self.mem1_addr == 0 and size == 0x02000000 and offset == 0:
-                    self.mem1_addr = start_addr
-                elif self.mem2_addr == 0 and size == 0x04000000 and offset == 0x02040000:
-                    self.mem2_addr = start_addr
-                # fmt: on
-                if self.mem1_addr != 0 and self.mem2_addr != 0:
-                    break
-        assert self.mem1_addr != 0 and self.mem2_addr != 0
+        raise ValueError()
 
-    def read_byte(self, address: int) -> bytes:
+    def get_offset(self, gcn_addr: int) -> int:
+        if 0x80000000 <= gcn_addr < 0x81800000:
+            return gcn_addr - 0x80000000
+        elif 0x90000000 <= gcn_addr < 0x94000000:
+            return (gcn_addr - 0x90000000) + 0x02040000
 
-        if self.MEM1_START <= address < self.MEM1_START + self.MEM1_SIZE:
-            host_addr = self.mem1_addr + (address - self.MEM1_START)
-        elif self.MEM2_START <= address < self.MEM2_START + self.MEM2_SIZE:
-            host_addr = self.mem2_addr + (address - self.MEM2_START)
-        else:
-            raise ValueError("Address out of range")
+        raise ValueError()
 
-        with open(f"/proc/{self.pid}/mem", "rb") as f:
-            f.seek(host_addr)
-            return f.read(1)
-
-    def write_byte(self, address: int, value: bytes):
-
-        if self.MEM1_START <= address < self.MEM1_START + self.MEM1_SIZE:
-            host_addr = self.mem1_addr + (address - self.MEM1_START)
-        elif self.MEM2_START <= address < self.MEM2_START + self.MEM2_SIZE:
-            host_addr = self.mem2_addr + (address - self.MEM2_START)
-        else:
-            raise ValueError("Address out of range")
-
-        with open(f"/proc/{self. pid}/mem", "r+b") as f:
-            f.seek(host_addr)
-            f.write(value)
+    def read_byte(self, addr: int) -> int:
+        offset = self.get_offset(addr)
+        return self.mv[offset]
