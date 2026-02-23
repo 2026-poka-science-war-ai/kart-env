803e80f1546aee7bb3eba0d3bf60fd062a2f898d
jae1u <yjw_0412@naver.com>
2026-02-23 04:30:46 +0000

    feat: Integrate game launch functionality and adjust free function frame count

--- kart_env/utils/helper.py
+++ kart_env/utils/helper.py
@@ -2,10 +2,14 @@ from .enums import NEUTRAL_ACTION
 from ..kart_environment import KartEnvironment
 
 
-def free(env: KartEnvironment, num_frame: int = 500):
+def free(env: KartEnvironment, num_frame: int = 1000):
     for _ in range(num_frame):
-        env.step({i: NEUTRAL_ACTION for i in range(4)})
+        env.step({})
 
 
 def exec_cmd(env: KartEnvironment, command: str):
     env.conn.sendall(b"exec" + command.encode())
+
+
+def launch_game(env: KartEnvironment):
+    free(env, num_frame=100)
