8558afeeb675359970df5fa6bfe5765b20cae3da
jae1u <yjw_0412@naver.com>
2026-03-29 14:26:41 +0000

    feat: Add OptionType in `__init__.py` and use it in `main.py`

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from kart_env import KartEnvironment
+from kart_env import KartEnvironment, OptionType
 
 if __name__ == "__main__":
-    env = KartEnvironment()
+    options = OptionType(num_agents=4)
+    env = KartEnvironment(options=options)
 
     observations, infos = env.reset()
 
-    for _ in range(5000):
+    for _ in range(500):
         actions = {agent: env.action_space(agent).sample() for agent in env.agents}
         observations, rewards, terminations, truncations, infos = env.step(actions)
 
--- src/kart_env/__init__.py
+++ src/kart_env/__init__.py
@@ -1,1 +1,2 @@
 from .kart_environment import KartEnvironment
+from .utils.macro_helper import OptionType
