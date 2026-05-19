ff3610d53b0d2a9f106360cbb3761e3d8153d8be
jae1u <yjw_0412@naver.com>
2026-05-19 17:11:04 +0000

    feat: Remove hardcoded NUM_SHM_FRAMES in KartGraphicObs class

--- src/kart_env/utils/kart_graphic_obs.py
+++ src/kart_env/utils/kart_graphic_obs.py
@@ -8,31 +8,34 @@ from PIL import Image
 class KartGraphicObs:
     def __init__(self, env_id: int):
         self.shm_name = f"DolphinSharedFrameBuffer_{env_id}"
-        self.NUM_SHM_FRAMES = 7
         self.HEADER_METADATA_SIZE = 40
         self.FRAME_INFO_SIZE = 32
-        self.TOTAL_HEADER_SIZE = self.HEADER_METADATA_SIZE + (
-            self.FRAME_INFO_SIZE * self.NUM_SHM_FRAMES
-        )
         self.EXPECTED_MAGIC = 0x444C464E
         self.VERSION = 2
 
         self.shm = shared_memory.SharedMemory(name=self.shm_name)
         resource_tracker.unregister("/" + self.shm.name, "shared_memory")
 
+        assert self.shm.buf is not None
+        initial_metadata = struct.unpack(
+            "IIIIQQII", self.shm.buf[: self.HEADER_METADATA_SIZE]
+        )
+        magic, version, num_frames, _, _, _, _, _ = initial_metadata
+
+        assert magic == self.EXPECTED_MAGIC and version == self.VERSION
+
+        self.num_frames = num_frames
+
     def get(self) -> List[np.ndarray]:
         assert self.shm.buf is not None
         buf = self.shm.buf
 
-        metadata = struct.unpack("IIIIQQII", buf[: self.HEADER_METADATA_SIZE])
-        magic, version, num_frames, ready, frame_number, timestamp, _, _ = metadata
-        assert magic == self.EXPECTED_MAGIC and version == self.VERSION
-
+        ready = struct.unpack("I", buf[12:16])[0]
         while ready == 0:
             ready = struct.unpack("I", buf[12:16])[0]
 
         images = []
-        for i in range(self.NUM_SHM_FRAMES):
+        for i in range(self.num_frames):
             offset = self.HEADER_METADATA_SIZE + (i * self.FRAME_INFO_SIZE)
             f_info = struct.unpack(
                 "IIIIIIII", buf[offset : offset + self.FRAME_INFO_SIZE]
@@ -46,7 +49,6 @@ class KartGraphicObs:
                 strides=(stride, 4, 1),
             )
             images.append(img_view)
-            # Maybe we need to copy the image, idk
 
         return images
 
